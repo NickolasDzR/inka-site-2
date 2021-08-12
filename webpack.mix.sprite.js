919d115885674bdfd88ba15ed115ffbbae0985f8
NickolasDzR <nikolyae2019@gmail.com>
2021-08-12 10:52:29 +0300

    Правки

--- webpack.mix.sprite.js
+++ webpack.mix.sprite.js
@@ -1,17 +1,19 @@
 let mix = require("laravel-mix");
 const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
+const path = require("path");
 
 mix.webpackConfig({
         plugins: [
             new SVGSpritemapPlugin('src/img/svg/**/*.svg', {
                 output: {
-                    filename: '/dist/img/sprites/sprite.svg',
+                    filename: '../dist/img/sprites/sprite.svg',
                     svgo: true,
                     svg4everybody: true,
+                    chunk: { keep: true }
                 },
                 sprite: {
                     prefix: "",
-                },
+                }
             }),
         ],
     },
