5e2bfbde7a3acdffde112c0ef9d9aab2c0e97529
NickolasDzR <nikolyae2019@gmail.com>
2021-08-12 19:27:38 +0300

    Правки согласно

--- webpack.mix.js
+++ webpack.mix.js
@@ -26,37 +26,37 @@ if (process.env.section) {
                 })
             ]
         })
-        .extractMediaQueries({
-            verbose: false,
-            minify: mix.inProduction(),
-            combined: true,
-            groups: [
-                {
-                    breakpoints: [
-                        {
-                            minWidth: 420,
-                            filename: `../dist/css/media-xs`,
-                        },
-                        {
-                            minWidth: 576,
-                            filename: `../dist/css/media-sm`,
-                        },
-                        {
-                            minWidth: 767,
-                            filename: `../dist/css/media-md`,
-                        },
-                        {
-                            minWidth: 992,
-                            filename: `../dist/css/media-lg`,
-                        },
-                        {
-                            minWidth: 1280,
-                            filename: `../dist/css/media-xl`,
-                        }
-                    ],
-                }
-            ]
-        })
+        // .extractMediaQueries({
+        //     verbose: false,
+        //     minify: mix.inProduction(),
+        //     combined: true,
+        //     groups: [
+        //         {
+        //             breakpoints: [
+        //                 {
+        //                     minWidth: 420,
+        //                     filename: `../dist/css/media-xs`,
+        //                 },
+        //                 {
+        //                     minWidth: 576,
+        //                     filename: `../dist/css/media-sm`,
+        //                 },
+        //                 {
+        //                     minWidth: 767,
+        //                     filename: `../dist/css/media-md`,
+        //                 },
+        //                 {
+        //                     minWidth: 992,
+        //                     filename: `../dist/css/media-lg`,
+        //                 },
+        //                 {
+        //                     minWidth: 1280,
+        //                     filename: `../dist/css/media-xl`,
+        //                 }
+        //             ],
+        //         }
+        //     ]
+        // })
         .sourceMaps()
         .pug("src/views/index.pug", "../../dist")
         .copyDirectoryWatched("./src/fonts", "./dist/fonts")
