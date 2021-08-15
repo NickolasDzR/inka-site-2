5ae3dfdd7b5f74fac97248b52c5e884389006276
NickolasDzR <nikolyae2019@gmail.com>
2021-08-15 04:44:01 +0300

    Правки

--- src/blocks/modules/cryptocurrencies/cryptocurrencies.js
+++ src/blocks/modules/cryptocurrencies/cryptocurrencies.js
@@ -6,77 +6,12 @@ const circle = document.querySelector(".cryptocurrencies__circle-inner"),
     cryptoAnimationTime = document.querySelector(".cryptocurrencies__controll").dataset.animationTime,
     cryptoAnimationInitBy = parseInt(document.querySelector(".cryptocurrencies__controll").dataset.animationHideCrypto);
 
-// const clearIntervalFunc = (variable) => {
-//     return window.clearInterval(variable);
-// };
-//
-// let addAnimationSetTimeout = undefined;
-// let removeAnimationSetTimeout = undefined
-//
-// const addAnimation = () => {
-//     return addAnimationSetTimeout = setInterval(() => {
-//         smallCircles.forEach((el, index) => {
-//             const activationAnimationClass = `cryptocurrencies__item-img_${index + 1}_transform`;
-//             if (!el.classList.contains(activationAnimationClass)) {
-//                 el.classList.add(activationAnimationClass);
-//                 cryptocurrenciesCircleInner.classList.add("cryptocurrencies__circle-inner_scale")
-//             }
-//         });
-//     }, 2000)
-// }
-//
-// const addClassesAfterClearInterval = () => {
-//     console.log("Asdasd");
-//     return smallCircles.forEach((el, index) => {
-//         const activationAnimationClass = `cryptocurrencies__item-img_${index + 1}_transform`;
-//         return el.classList.add(activationAnimationClass);
-//     });
-// }
-//
-// const removeAnimation = () => {
-//     return removeAnimationSetTimeout = setInterval(() => {
-//         smallCircles.forEach((el, index) => {
-//             const activationAnimationClass = `cryptocurrencies__item-img_${index + 1}_transform`;
-//             if (el.classList.contains(activationAnimationClass)) {
-//                 el.classList.remove(activationAnimationClass);
-//                 cryptocurrenciesCircleInner.classList.remove("cryptocurrencies__circle-inner_scale")
-//             }
-//         });
-//     }, 2000)
-// };
-//
-// function cryptoAnimationControll() {
-//     const animationTime = cryptoAnimationTime,
-//         cryptocurrenciesCircle = document.querySelector(".cryptocurrencies__circle"),
-//         cryptocurrenciesCircleInner = document.querySelector(".cryptocurrencies__circle-cover"),
-//         cryptocurrenciesSmallImg = document.querySelectorAll(".cryptocurrencies__cryptos");
-//
-//     [cryptocurrenciesCircle, cryptocurrenciesCircleInner, ...cryptocurrenciesSmallImg].forEach(el => {
-//         el.style.animationDuration = `${animationTime}s`
-//     });
-//     addAnimation();
-//     setTimeout(removeAnimation, 1000);
-// }
-//
-// cryptoAnimationControll();
-//
-// window.onfocus = () => {
-//     addAnimation();
-//     setTimeout(removeAnimation, 1000);
-// };
-//
-// window.onblur = () => {
-//     clearIntervalFunc(addAnimationSetTimeout)
-//     clearIntervalFunc(removeAnimationSetTimeout)
-//     return addClassesAfterClearInterval();
-// }
-
 const addAnimation = () => {
     smallCircles.forEach((el, index) => {
         const activationAnimationClass = `cryptocurrencies__item-img_${index + 1}_transform`;
         if (!el.classList.contains(activationAnimationClass)) {
             el.classList.add(activationAnimationClass);
-            cryptocurrenciesCircleInner.classList.add("cryptocurrencies__circle-inner_scale")
+            return cryptocurrenciesCircleInner.classList.add("cryptocurrencies__circle-inner_scale")
         }
     });
 }
@@ -84,10 +19,8 @@ const addAnimation = () => {
 const removeAnimation = () => {
     smallCircles.forEach((el, index) => {
         const activationAnimationClass = `cryptocurrencies__item-img_${index + 1}_transform`;
-        if (el.classList.contains(activationAnimationClass)) {
-            el.classList.remove(activationAnimationClass);
-            cryptocurrenciesCircleInner.classList.remove("cryptocurrencies__circle-inner_scale")
-        }
+        el.classList.remove(activationAnimationClass);
+        return cryptocurrenciesCircleInner.classList.remove("cryptocurrencies__circle-inner_scale")
     });
 };
 
@@ -102,7 +35,8 @@ function cryptoAnimationControll() {
     });
 }
 
-gsap.set(removeAnimation, {delay: 0, onRepeat: removeAnimation, duration: 2, repeat: -1, repeatDelay: 4});
-gsap.set(addAnimation, {delay: 2, onRepeat: addAnimation, duration: 2, repeat: -1, repeatDelay: 4});
+gsap.set(removeAnimation, {delay: 0, onRepeat: removeAnimation, duration: 4, repeat: -1, repeatDelay: 1});
+
+gsap.set(addAnimation, {delay: 0, onRepeat: addAnimation, duration: 1, repeat: -1, repeatDelay: 4});
 
 cryptoAnimationControll();
