a283ec573fff2ffd1c808a44709cb8c566af20e9
NickolasDzR <nikolyae2019@gmail.com>
2021-08-15 04:01:25 +0300

    Правки

--- src/blocks/modules/cryptocurrencies/cryptocurrencies.js
+++ src/blocks/modules/cryptocurrencies/cryptocurrencies.js
@@ -1,46 +1,94 @@
+import gsap from "gsap";
+
 const circle = document.querySelector(".cryptocurrencies__circle-inner"),
     smallCircles = document.querySelectorAll(".cryptocurrencies__item-img"),
     cryptocurrenciesCircleInner = document.querySelector(".cryptocurrencies__circle-inner"),
     cryptoAnimationTime = document.querySelector(".cryptocurrencies__controll").dataset.animationTime,
     cryptoAnimationInitBy = parseInt(document.querySelector(".cryptocurrencies__controll").dataset.animationHideCrypto);
 
-const clearIntervalFunc = (variable) => {
-    return window.clearInterval(variable);
-};
-
-let addAnimationSetTimeout = undefined;
-let removeAnimationSetTimeout = undefined
+// const clearIntervalFunc = (variable) => {
+//     return window.clearInterval(variable);
+// };
+//
+// let addAnimationSetTimeout = undefined;
+// let removeAnimationSetTimeout = undefined
+//
+// const addAnimation = () => {
+//     return addAnimationSetTimeout = setInterval(() => {
+//         smallCircles.forEach((el, index) => {
+//             const activationAnimationClass = `cryptocurrencies__item-img_${index + 1}_transform`;
+//             if (!el.classList.contains(activationAnimationClass)) {
+//                 el.classList.add(activationAnimationClass);
+//                 cryptocurrenciesCircleInner.classList.add("cryptocurrencies__circle-inner_scale")
+//             }
+//         });
+//     }, 2000)
+// }
+//
+// const addClassesAfterClearInterval = () => {
+//     console.log("Asdasd");
+//     return smallCircles.forEach((el, index) => {
+//         const activationAnimationClass = `cryptocurrencies__item-img_${index + 1}_transform`;
+//         return el.classList.add(activationAnimationClass);
+//     });
+// }
+//
+// const removeAnimation = () => {
+//     return removeAnimationSetTimeout = setInterval(() => {
+//         smallCircles.forEach((el, index) => {
+//             const activationAnimationClass = `cryptocurrencies__item-img_${index + 1}_transform`;
+//             if (el.classList.contains(activationAnimationClass)) {
+//                 el.classList.remove(activationAnimationClass);
+//                 cryptocurrenciesCircleInner.classList.remove("cryptocurrencies__circle-inner_scale")
+//             }
+//         });
+//     }, 2000)
+// };
+//
+// function cryptoAnimationControll() {
+//     const animationTime = cryptoAnimationTime,
+//         cryptocurrenciesCircle = document.querySelector(".cryptocurrencies__circle"),
+//         cryptocurrenciesCircleInner = document.querySelector(".cryptocurrencies__circle-cover"),
+//         cryptocurrenciesSmallImg = document.querySelectorAll(".cryptocurrencies__cryptos");
+//
+//     [cryptocurrenciesCircle, cryptocurrenciesCircleInner, ...cryptocurrenciesSmallImg].forEach(el => {
+//         el.style.animationDuration = `${animationTime}s`
+//     });
+//     addAnimation();
+//     setTimeout(removeAnimation, 1000);
+// }
+//
+// cryptoAnimationControll();
+//
+// window.onfocus = () => {
+//     addAnimation();
+//     setTimeout(removeAnimation, 1000);
+// };
+//
+// window.onblur = () => {
+//     clearIntervalFunc(addAnimationSetTimeout)
+//     clearIntervalFunc(removeAnimationSetTimeout)
+//     return addClassesAfterClearInterval();
+// }
 
 const addAnimation = () => {
-    return addAnimationSetTimeout = setInterval(() => {
-        smallCircles.forEach((el, index) => {
-            const activationAnimationClass = `cryptocurrencies__item-img_${index + 1}_transform`;
-            if (!el.classList.contains(activationAnimationClass)) {
-                el.classList.add(activationAnimationClass);
-                cryptocurrenciesCircleInner.classList.add("cryptocurrencies__circle-inner_scale")
-            }
-        });
-    }, 2000)
-}
-
-const addClassesAfterClearInterval = () => {
-    console.log("Asdasd");
-    return smallCircles.forEach((el, index) => {
+    smallCircles.forEach((el, index) => {
         const activationAnimationClass = `cryptocurrencies__item-img_${index + 1}_transform`;
-        return el.classList.add(activationAnimationClass);
+        if (!el.classList.contains(activationAnimationClass)) {
+            el.classList.add(activationAnimationClass);
+            cryptocurrenciesCircleInner.classList.add("cryptocurrencies__circle-inner_scale")
+        }
     });
 }
 
 const removeAnimation = () => {
-    return removeAnimationSetTimeout = setInterval(() => {
-        smallCircles.forEach((el, index) => {
-            const activationAnimationClass = `cryptocurrencies__item-img_${index + 1}_transform`;
-            if (el.classList.contains(activationAnimationClass)) {
-                el.classList.remove(activationAnimationClass);
-                cryptocurrenciesCircleInner.classList.remove("cryptocurrencies__circle-inner_scale")
-            }
-        });
-    }, 2000)
+    smallCircles.forEach((el, index) => {
+        const activationAnimationClass = `cryptocurrencies__item-img_${index + 1}_transform`;
+        if (el.classList.contains(activationAnimationClass)) {
+            el.classList.remove(activationAnimationClass);
+            cryptocurrenciesCircleInner.classList.remove("cryptocurrencies__circle-inner_scale")
+        }
+    });
 };
 
 function cryptoAnimationControll() {
@@ -52,19 +100,9 @@ function cryptoAnimationControll() {
     [cryptocurrenciesCircle, cryptocurrenciesCircleInner, ...cryptocurrenciesSmallImg].forEach(el => {
         el.style.animationDuration = `${animationTime}s`
     });
-    addAnimation();
-    setTimeout(removeAnimation, 1000);
 }
+
+gsap.set(removeAnimation, {delay: 0, onRepeat: removeAnimation, duration: 2, repeat: -1, repeatDelay: 4});
+gsap.set(addAnimation, {delay: 2, onRepeat: addAnimation, duration: 2, repeat: -1, repeatDelay: 4});
 
 cryptoAnimationControll();
-
-window.onfocus = () => {
-    addAnimation();
-    setTimeout(removeAnimation, 1000);
-};
-
-window.onblur = () => {
-    clearIntervalFunc(addAnimationSetTimeout)
-    clearIntervalFunc(removeAnimationSetTimeout)
-    return addClassesAfterClearInterval();
-}
