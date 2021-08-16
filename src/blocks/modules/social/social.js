12668350265fde7178ecec3a5bfd20c5c5ac5a54
NickolasDzR <nikolyae2019@gmail.com>
2021-08-16 23:10:24 +0300

    Правки

--- src/blocks/modules/social/social.js
+++ src/blocks/modules/social/social.js
@@ -5,25 +5,38 @@ const popper = document.querySelector(".social-popup");
 
 const popperSettings = {
     placement: "top",
-    modifiers: [
-        {
-            name: 'preventOverflow',
-            options: {
-                margin: 10
-            },
-        },
-    ]
 };
+
+let popperInit = undefined;
+
+const showTooltip = (el) => {
+    console.log("enter");
+    popper.setAttribute("data-show", '');
+
+    if (popperInit === undefined) {
+        popperInit = createPopper(el.target, popper, popperSettings);
+        popperInit.update();
+        return false
+    }
+};
+
+const hideTooltip = (el) => {
+    popperInit.destroy();
+    popperInit = undefined;
+    popper.removeAttribute('data-show');
+};
+
+const showEvents = ['mouseenter', 'focus'];
+const hideEvents = ['mouseleave', 'blur'];
 
 socLinks.forEach(el => {
     if (el.href.length === 0) {
+        showEvents.forEach(event => {
+            el.addEventListener(event, showTooltip)
+        });
 
-        el.addEventListener("click", function () {
-            createPopper(el, popper, popperSettings);
-            setTimeout(() => {
-                popper.removeAttribute("data-popper-placement");
-                popper.removeAttribute("style");
-            }, 3000)
-        })
+        hideEvents.forEach(event => {
+            el.addEventListener(event, hideTooltip)
+        });
     }
-})
+});
